5031759141135d4028799d2a3c6d4bcf016cd4a0
smschijf <210578@student.glu.nl>
2022-04-01 14:09:12 +0200

    front-end login and signup page done

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>admin</title>
+  <link rel="stylesheet" href="assets/css/login.css">
+</head>
+<body>
+  <div id="container">
+    <div class="form-container">
+      <form method="post">
+        <h3>Login</h3>
+        <div class="input-label">Username</div>
+        <input type="text" name="user_name">
+        <div class="input-label">Password</div>
+        <input type="password" name="password">
+        <input type="submit" value="Login">
+        <a href="php/signup.php">Signup instead</a>
+      </form>
+    </div>
+  </div>
+</body>
+</html>
---
+++ php/signup.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>admin</title>
+  <link rel="stylesheet" href="../assets/css/signup.css">
+</head>
+<body>
+  <div id="container">
+    <div class="form-container">
+      <form method="post">
+        <h3>Signup</h3>
+        <div class="input-label">First Name</div>
+        <input type="text" name="first_name">
+        <div class="input-label">Middle name</div>
+        <input type="text" name="middle_name">
+        <div class="input-label">Last Name</div>
+        <input type="text" name="last_name">
+        <div class="input-label">City</div>
+        <input type="text" name="city">
+        <div class="input-label">Street Name</div>
+        <input type="text" name="street_name">
+        <div class="input-label">House Number</div>
+        <input type="number" name="house_number">
+        <div class="input-label">Email address</div>
+        <input type="text" name="email_address">
+        <div class="input-label">Username</div>
+        <input type="text" name="user_name">
+        <div class="input-label">Password</div>
+        <input type="password" name="password">
+        <input type="submit" value="Signup">
+        <a href="../index.php">Login instead</a>
+      </form>
+    </div>
+  </div>
+</body>
+</html>
